32b954dd7a149faaa99734a4c98151c9e8d2b848
박현우 <simuoo96@gmail.com>
2024-01-17 19:25:28 +0900

    docs : MyPage SafeArea 적용

--- lib/AET/screens/mypage.dart
+++ lib/AET/screens/mypage.dart
@@ -5,7 +5,6 @@ import 'package:aet/AET/profile/profile_photo_card.dart';
 import 'package:aet/AET/profile/profile_photo_name.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -14,34 +13,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: CustomAppbarProfile(
-          titleWrite: '프로필',),
-        body: Container(
-          color: Color(0xFFf0f0f0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(10,20,10,0),
-                child: ProfileName(),
+        debugShowCheckedModeBanner: false,
+        home: SafeArea(
+          child: Scaffold(
+            appBar: CustomAppbarProfile(
+              titleWrite: '프로필',),
+            body: Container(
+              color: Color(0xFFf0f0f0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10,20,10,0),
+                    child: ProfileName(),
+                  ),
+                  ProfileCard(),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10,20,10,10),
+                    child: ProfileManagerBar(),
+                  ),
+                  CustomList(textWrite: "시설등록"),
+                  CustomList(textWrite: "친구초대"),
+                  CustomList(textWrite: "1:1문의"),
+                  CustomList(textWrite: "자주하는질문"),
+                  CustomList(textWrite: "공지/이벤트"),
+                  CustomList(textWrite: "설정"),
+                ],
               ),
-              ProfileCard(),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10,20,10,10),
-                child: ProfileManagerBar(),
-              ),
-              CustomList(textWrite: "시설등록"),
-              CustomList(textWrite: "친구초대"),
-              CustomList(textWrite: "1:1문의"),
-              CustomList(textWrite: "자주하는질문"),
-              CustomList(textWrite: "공지/이벤트"),
-              CustomList(textWrite: "설정"),
-            ],
+            ),
           ),
-        ),
-      ),
+        )
     );
   }
 }
